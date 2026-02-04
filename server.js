e75aea62675117a6e3707f5d2f8de2dd53dad91c
ahmed kammoun <ahmed.kammoun@enis.tn>
2026-02-04 23:30:45 +0100

    Fix CI/CD pipeline for self-hosted runner

--- server.js
+++ server.js
@@ -102,6 +102,6 @@ app.delete('/api/users/:id', async (req, res) => {
 // Start the server
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
-    console.log('CI/CD pipeline is active!+Kubernetes deployment is successful! + sans jenkins');
+    console.log('CI/CD pipeline is active!+Kubernetes deployment is successful! + self-hosted runner + sans jenkins');
 });
 module.exports = app;
