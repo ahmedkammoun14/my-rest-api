ad7db6c681aef89beab64724e3bec6d01935464b
ahmed kammoun <ahmed.kammoun@enis.tn>
2026-02-07 14:48:51 +0100

    Jenkins 07/02/2026 26

--- server.js
+++ server.js
@@ -156,7 +156,7 @@ async function startServer() {
         console.log('✅ CI/CD pipeline is active!');
         console.log('✅ Kubernetes deployment is successful!');
         console.log('✅ Database connection established!');
-        console.log('✅ Jenkins Reussi ---------!');
+        console.log('✅ ------------Jenkins Reussi ---------!');
 
     });
 }
